2c06cfb1b3399b9afc932a053d1d367075b7f505
henok3878 <henokwondimu3878@gmail.com>
2025-05-04 00:58:38 -0400

    feat(data): add static method to return alphabets in the dataset

--- src/data/dataloader.py
+++ src/data/dataloader.py
@@ -12,6 +12,14 @@ class ProcessedHandwritingDataset(Dataset):
         self.writer_ids = np.load(os.path.join(processed_dir, "writer_ids.npy"))
         self.mask = np.load(os.path.join(processed_dir, "mask.npy"))
     
+    @staticmethod
+    def get_alphabet():
+        return ['\x00',' ', '!', '"', '#', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/', '0', '1', '2', '3', '4', 
+                    '5', '6', '7', '8', '9', ':', ';', '?', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 
+                    'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '[', ']', 'a', 'b', 
+                    'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 
+                    'v', 'w', 'x', 'y', 'z'] 
+    
     def __len__(self):
         return self.strokes.shape[0] 
     
