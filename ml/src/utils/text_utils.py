f08600a232917ac9f46905ea1297c1674b847250
henok3878 <henokwondimu3878@gmail.com>
2025-05-30 18:07:55 -0400

    feat(ml utils): add text_utils for alphabet and text encoding

---
+++ ml/src/utils/text_utils.py
@@ -0,0 +1,30 @@
+from typing import Dict
+import numpy as np 
+
+NULL_CHAR = '\x00'
+
+def construct_alphabet_list(alphabet_string: str) -> list[str]:
+    if not isinstance(alphabet_string, str):
+        raise TypeError("alphabet_string must be a string") 
+    
+    char_list = list(alphabet_string) 
+    return [NULL_CHAR] + char_list 
+
+def get_alphabet_map(alphabet_list: list[str]) -> Dict[str, int]:
+    """creates a char to index map from full alphabet list"""
+    return {char: idx for idx, char in enumerate(alphabet_list)}  
+
+def encode_text(text: str, char_to_index_map: Dict[str, int], 
+                max_length: int, add_eos: bool = True, eos_char_index: int = 0
+                ) -> np.ndarray:
+    """Enocde a text string into a sequence of integer indices"""
+    encoded = [char_to_index_map.get(c, eos_char_index) for c in text] 
+    if add_eos:
+        encoded.append(eos_char_index) 
+    if len(encoded) < max_length: 
+        padded_encoded = np.full(max_length, eos_char_index, dtype=np.int64) 
+        padded_encoded[:len(encoded)] = encoded 
+    else:
+        padded_encoded = np.array(encoded[:max_length], dtype=np.int64) 
+    
+    return padded_encoded
