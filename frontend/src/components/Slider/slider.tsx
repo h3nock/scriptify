eb93ec410326c09cc15a76d78c7183d6afeb1dd9
henok3878 <henokwondimu3878@gmail.com>
2025-06-07 12:58:14 -0400

    feature(components): implement slider component

---
+++ frontend/src/components/Slider/slider.tsx
@@ -0,0 +1,70 @@
+import React from 'react'; 
+import './slider.css'; 
+
+interface SliderProps {
+    label: string; 
+    value: number; 
+    min: number; 
+    max: number; 
+    step: number; 
+    onChange: (value: number) => void; 
+    getLabel: (value: number) => string; 
+    leftLabel: string; 
+    rightLabel: string; 
+    disabled?: boolean; 
+    inverted?: boolean; 
+}
+
+const Slider: React.FC<SliderProps> = ({
+  label,
+  value,
+  min,
+  max,
+  step,
+  onChange,
+  getLabel,
+  leftLabel,
+  rightLabel,
+  disabled = false,
+  inverted = false,
+}) => {
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const newValue = Number(e.target.value);
+    if (inverted) {
+      onChange((min + max) - newValue);
+    } else {
+      onChange(newValue);
+    }
+  };
+
+  const displayValue = inverted ? (min + max) - value : value;
+
+  return (
+    <div className="setting-group">
+      <label>
+        {label}:{" "}
+        <span className="setting-value">
+          {getLabel(value)}
+        </span>
+      </label>
+      <div className="slider-container">
+        <input
+          type="range"
+          min={min}
+          max={max}
+          step={step}
+          value={displayValue}
+          onChange={handleChange}
+          disabled={disabled}
+          className="slider"
+        />
+        <div className="slider-labels">
+          <span>{leftLabel}</span>
+          <span>{rightLabel}</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Slider; 
